a90fb53cb5577fe09ba648c689e76f5bcef475a1
hemasendil <hpsendil150106@gmail.com>
2025-11-23 11:54:47 +0530

    Added new implementation to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
 
 
 
